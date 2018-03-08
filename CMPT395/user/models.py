2a11895714e4762f7d72c6a442a101408955c8f9
Tristan Bryson <brysont3@mymacewan.ca>
2018-03-08 12:49:41 -0700

    Updating the new models
    Small change that added a classroom to the signup onject for easy
    display.

--- CMPT395/user/models.py
+++ CMPT395/user/models.py
@@ -47,7 +47,7 @@ class Child(models.Model):
     FamilyID = models.ForeignKey(Family, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=25)
     family_name = models.CharField(max_length=25)
-    classroom = models.CharField(max_length=20)
+    classroom = models.CharField(max_length=15)
 
     def __str__(self):
         return '%s %s' % (self.first_name, self.family_name)
@@ -69,6 +69,7 @@ class Signup(models.Model):
     SignupID = models.AutoField(primary_key=True)
     date = models.DateField()
     start_time = models.TimeField()
+    classroom = models.CharField(max_length=15)
     VolunteerID = models.ForeignKey(Volunteer, on_delete= models.CASCADE)
 
     def __str__(self):
