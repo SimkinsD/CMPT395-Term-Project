c60980eddea333f30dd62e77759e97c3c0cf0e7c
Tristan Bryson <brysont3@mymacewan.ca>
2018-03-27 15:40:25 -0600

    Gets specific families weekly, monthly, and requried hours
    Can now get weekly and monthly hours for induvidual families as well
    as their required hours for the week and month. Still unable to
    use the currently logged in user.

--- CMPT395/stats/views.py
+++ CMPT395/stats/views.py
@@ -8,21 +8,33 @@ class FamilyStats(generic.ListView):
     template_name = 'family_stats.html'
     model = Signup
 
+
     def __init__(self):
-        self.total = self.week_hours()
+        self.week_total = self.week_hours(datetime.date.today(), Family.objects.get(user=5))#self.request.user))
+        self.month_total = self.month_hours(datetime.date.today(), Family.objects.get(user=5))#self.request.user))
+        self.required_hours = self.required_hours(Family.objects.get(user=4))#self.request.user))
+
+
 
     """ Using the current date it gets the entire weeks hours for selected family.
-        It returns a timedelta object of the current hours and minutes volunteered that week.
+        Return:
+            It returns a timedelta object of the current hours and minutes volunteered that week.
+        Param: 
+            requested_week: is the week you would like the volunteer hours for.
+                It must be a datetme.date object of a single day in the requested week.
+            requested_family: is the family that you want the weekly hours for.
+                It must be a single Family object (from user.models) 
     """
-    def week_hours(self):
+    def week_hours(self, requested_week, requested_family):
         total = datetime.timedelta(hours=0)
-        week = datetime.date.today()
+        week = requested_week
         offset = week.weekday()
         # Gets the beginning and end dates based on current date
         begin_week =  week - datetime.timedelta(days=offset)
         end_week = week + datetime.timedelta(days=(6 - offset))
-        family = Family.objects.get(user=5)#self.request.user)
-        signups = Signup.objects.filter(volunteer__family__familyID = family.familyID)
+        family = requested_family
+        signups = Signup.objects.filter(volunteer__family__familyID = family.familyID, 
+                                        date__range=(begin_week, end_week))
 
         # Goes through each signup for the family and calculates total hours for the week
         for day in signups:
@@ -30,6 +42,57 @@ class FamilyStats(generic.ListView):
             start = datetime.timedelta(hours=day.start_time.hour,minutes=day.start_time.minute)
             total = total + (end - start)
         return total
+
+
+
+    """ Using the current date it gets the entire month hours for selected family.
+        Return:
+            It returns a timedelta object of the current hours and minutes volunteered that month.
+        Param: 
+            requested_month: is the month you would like the volunteer hours for.
+                It must be a datetme.date object of a single day in the requested month.
+            requested_family: is the family that you want the monthly hours for.
+                It must be a single Family object (from user.models)
+    """
+    def month_hours(self, requested_month, requested_family):
+        total = datetime.timedelta(hours=0)
+        current = requested_month
+        family = requested_family
+        signups = Signup.objects.filter(volunteer__family__familyID = family.familyID, 
+                                        date__year=current.year, date__month=current.month)
+        # Goes through each signup for the family and calculates total hours for the month
+        for day in signups:
+            end = datetime.timedelta(hours=day.end_time.hour,minutes=day.end_time.minute)
+            start = datetime.timedelta(hours=day.start_time.hour,minutes=day.start_time.minute)
+            total = total + (end - start)
+        return total
+
+
+    """ This functions return the required hours a family must complete (as an integer)
+        in a few depending on how many children they have. 
+        Return:
+            It returns an int of the amount of hours the family must complete
+        Param:
+            requested_family: is the family that you want the number of children for.
+                It must be a single Family object (from user.models)
+    """
+    def required_hours(self, requested_family):
+        children = (Child.objects.filter(family__familyID = requested_family.familyID).count())
+        return (2.5 if children < 2 else 5)
+
+
+
+
+class AdminStats(generic.ListView):
+    template_name = "admin_stats.html"
+    model = Signup
+
+
+    def __init__(self):
+        pass
+
+
+
 
 
 
