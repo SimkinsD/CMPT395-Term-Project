813c0b13af3812ac7d1ad03dd781e6b6e8501631
John_Chubey <johnchubey@gmail.com>
2018-03-28 20:25:16 -0600

    Add max_length to textField on request time off form. Also give TimeOffRequests a string representation.

--- CMPT395/TimeOffRequests/models.py
+++ CMPT395/TimeOffRequests/models.py
@@ -5,4 +5,6 @@ class TimeOffRequest(models.Model):
     # family = models.ForeignKey(Family, on_delete = models.CASCADE)
     start_date = models.DateField(blank = False)
     end_date = models.DateField(blank = False)
-    reason_for_time_off = models.TextField()
+    reason_for_time_off = models.TextField(max_length = 300)
+    def __str__(self):
+        return "TimeOffRequest| Start date:" + str(self.start_date) + "| End Date:" + str(self.end_date) + "\n"
