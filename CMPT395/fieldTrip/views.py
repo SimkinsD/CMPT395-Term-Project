03994fb8f60b5ed0d879009a2974dcb2849ddf85
Tristan Bryson <brysont3@mymacewan.ca>
2018-04-04 21:42:24 -0600

    Changing the field trip signups

--- CMPT395/fieldTrip/views.py
+++ CMPT395/fieldTrip/views.py
@@ -4,17 +4,16 @@ from django.views.generic.edit import CreateView
 from django.views.generic import ListView
 from django.urls import reverse_lazy
 from . models import *
+from . forms import *
 from user.models import *
 import datetime
 
 
 
 class CreateFieldTrip(CreateView):
-    model = FieldTrip
+    form_class = CreateFieldTripForm
     success_url = reverse_lazy('create_field_trip')
     template_name = "field_trip.html"
-    fields = ('title', 'location', 'info', 'date')
-    widgets = {'date' : forms.DateInput}
 
 
 
@@ -22,17 +21,13 @@ class CurrentTrip(ListView):
     model = FieldTrip
     template_name = 'current_field_trips.html'
     context_object_name = 'trip_list'
-    current = datetime.date.today()
-    queryset = FieldTrip.objects.filter(date__gte=current)
-
 
 
 # Needs work
 class SignupFieldTrip(CreateView):
-    model = FieldTripSignup
+    form_class = SignupFieldTripForm
     success_url = reverse_lazy('signup_field_trip')
     template_name = 'signup_field_trip.html'
-    fields = ('trip', 'volunteer', 'classroom')
 
 
 
