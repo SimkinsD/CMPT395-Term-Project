b2b3bdc6a81e94bd2f8467cb4bc9d033f02c3df6
Tristan Bryson <brysont3@mymacewan.ca>
2018-04-04 22:28:56 -0600

    Updated test data

--- CMPT395/testdata.sql
+++ CMPT395/testdata.sql
@@ -1,78 +1,114 @@
 insert into 395db.user_myuser
-(id, password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
-values (1, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 1, 'admin', 'John', 'Doe', 'email@email.com', 1, 1, '2018-03-25 19:50:40.005625'),
-(2, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family1', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(3, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family2', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(4, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family4', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(5, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family3', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(6, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family14', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(7, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family5', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(8, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family6', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(9, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family7', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(10, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family8', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(11, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family9', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(12, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family10', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(13, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family11', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(14, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family12', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
-(15, 'pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'family13', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625');
+(password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+values ('pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 1, 'admin', 'John', 'Doe', 'email@email.com', 1, 1, '2018-03-25 19:50:40.005625'),
+('pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'smith1', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
+('pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'jones1', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
+('pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'trump1', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
+('pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'doe1', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
+('pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'smith2', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
+('pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'sedin1', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
+('pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'naslund1', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
+('pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'linden1', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625'),
+('pbkdf2_sha256$100000$fkQ5BjtLLXBu$YuIg3NypmH5KUISO30IHMHDM6dRVU4qNHL0n72hEBhs=', 0, 'crosby1', 'first', 'last', 'email@email.com', 0, 1, '2018-03-25 19:50:40.005625');
 
 insert into 395db.user_family (familyID, family_name, email, user_id)
-values (1, 'admin', 'email@email.com', 1),
-(2, 'family1', 'email@email.com', 2),
-(3, 'family2', 'email@email.com', 3),
-(4, 'family3', 'email@email.com', 4),
-(5, 'family4', 'email@email.com', 5),
-(6, 'family5', 'email@email.com', 6),
-(7, 'family6', 'email@email.com', 7),
-(8, 'family7', 'email@email.com', 8),
-(9, 'family8', 'email@email.com', 9),
-(10, 'family9', 'email@email.com', 10);
+values ('Teacher', 'email@email.com', 1),
+('Smith', 'email@email.com', 2),
+('Jones', 'email@email.com', 3),
+('Trump', 'email@email.com', 4),
+('Doe', 'email@email.com', 5),
+('Smith', 'email@email.com', 6),
+('Sedin', 'email@email.com', 7),
+('Naslund', 'email@email.com', 8),
+('Linden', 'email@email.com', 9),
+('Crosby', 'email@email.com', 10);
 
-insert into 395db.user_volunteer (volunteerID, first_name, last_name, email, family_id)
-values (3, 'volunteer1', 'Smith', 'email@email.com', 2),
-(4, 'volunteer1', 'Smith', 'email@email.com', 3),
-(5, 'volunteer1', 'Smith', 'email@email.com', 4),
-(6, 'volunteer1', 'Smith', 'email@email.com', 5),
-(7, 'volunteer2', 'Smith', 'email@email.com', 5),
-(8, 'volunteer1', 'Smith', 'email@email.com', 6),
-(9, 'volunteer1', 'Smith', 'email@email.com', 7),
-(10, 'volunteer1', 'Smith', 'email@email.com', 8);
-
-
-insert into 395db.user_child (childID, first_name, last_name, classroom, family_id)
-values (1, 'child1', 'last1', 'red', 2),
-(2, 'child2', 'last1', 'blue', 2),
-(3, 'child1', 'last3', 'red', 3),
-(4, 'child1', 'last4', 'red', 4),
-(5, 'child1', 'last5', 'red', 5),
-(6, 'child1', 'last6', 'red', 6),
-(7, 'child1', 'last7', 'red', 7),
-(8, 'child1', 'last8', 'green', 8),
-(9, 'child2', 'last8', 'grey', 8),
-(10, 'child3', 'last8', 'blue', 8),
-(11, 'child4', 'last8', 'blue', 8);
+insert into 395db.user_volunteer (first_name, last_name, email, family_id)
+values ('Tom', 'Smith', 'email@email.com', 2),
+('Jack', 'Jones', 'email@email.com', 3),
+('Donald', 'Smith', 'email@email.com', 4),
+('John', 'Doe', 'email@email.com', 5),
+('Dianna', 'Black', 'email@email.com', 5),
+('Tina', 'Smith', 'email@email.com', 6),
+('Daniel', 'Sedin', 'email@email.com', 7),
+('Markus', 'Naslund', 'email@email.com', 8),
+('Henrik', 'Sedin', 'email@email.com', 7);
 
 
+insert into 395db.user_child (first_name, last_name, classroom, family_id)
+values ('Timmy', 'Smith', 'red', 2),
+('Jane', 'Smith', 'blue', 2),
+('Tommy', 'Jones', 'red', 3),
+('Jr.', 'Trump', 'green', 4),
+('Jane', 'Doe', 'blue', 5),
+('Charlie', 'Smith', 'red', 6),
+('Sally', 'Sedin', 'blue', 7),
+('Chuck', 'Naslund', 'green', 8),
+('Jim', 'Naslund', 'green', 8),
+('Stacy', 'Park', 'blue', 8),
+('Sally', 'Bond', 'red', 8);
 
-insert into 395db.user_signup (signupID, date, start_time, end_time, classroom, volunteer_id)
-values (1, '2018-03-25', '8:30:00', '9:00:00', 'red', 3),
-(2, '2018-04-05', '8:30:00', '9:00:00', 'red', 4),
-(3, '2018-04-05', '8:30:00', '9:00:00', 'blue', 5),
-(4, '2018-04-05', '8:30:00', '9:00:00', 'red', 6),
-(5, '2018-04-05', '8:30:00', '9:00:00', 'yellow', 7),
-(6, '2018-04-05', '12:00:00', '13:00:00', 'red', 8),
-(7, '2018-04-05', '12:00:00', '13:00:00', 'blue', 9),
-(8, '2018-04-05', '12:00:00', '13:00:00', 'red', 10),
-(9, '2018-04-05', '13:30:00', '15:00:00', 'grey', 6),
-(10, '2018-04-05', '13:30:00', '15:00:00', 'red', 5),
-(11, '2018-04-05', '13:30:00', '15:00:00', 'purple', 4),
-(12, '2018-04-05', '8:30:00', '9:00:00', 'red', 10),
-(13, '2018-04-05', '13:30:00', '15:00:00', 'red', 3);
+
+
+insert into 395db.user_signup (date, start_time, end_time, classroom, volunteer_id)
+values ('2018-04-04', '8:45:00', '11:00:00', 'red', 3),
+('2018-04-04', '8:45:00', '10:00:00', 'green', 4),
+('2018-04-04', '8:45:00', '12:00:00', 'blue', 5),
+('2018-04-04', '12:50:00', '15:00:00', 'red', 6),
+('2018-04-04', '13:00:00', '15:45:00', 'blue', 7),
+('2018-04-04', '12:00:00', '13:00:00', 'red', 8),
+('2018-04-04', '12:00:00', '13:00:00', 'blue', 9),
+('2018-04-04', '12:00:00', '13:00:00', 'green', 1),
+('2018-04-04', '13:30:00', '15:30:00', 'green', 2),
+('2018-04-05', '8:45:00', '11:00:00', 'red', 3),
+('2018-04-05', '8:45:00', '10:00:00', 'green', 4),
+('2018-04-05', '8:45:00', '12:00:00', 'blue', 5),
+('2018-04-05', '12:50:00', '15:00:00', 'red', 6),
+('2018-04-05', '13:00:00', '15:45:00', 'blue', 7),
+('2018-04-05', '12:00:00', '13:00:00', 'red', 8),
+('2018-04-05', '12:00:00', '13:00:00', 'blue', 9),
+('2018-04-05', '12:00:00', '13:00:00', 'green', 1),
+('2018-04-05', '13:30:00', '15:30:00', 'green', 2),
+('2018-04-06', '8:45:00', '11:00:00', 'red', 3),
+('2018-04-06', '8:45:00', '10:00:00', 'green', 4),
+('2018-04-06', '8:45:00', '12:00:00', 'blue', 5),
+('2018-04-06', '12:50:00', '15:00:00', 'red', 6),
+('2018-04-06', '13:00:00', '15:45:00', 'blue', 7),
+('2018-04-06', '12:00:00', '13:00:00', 'red', 8),
+('2018-04-06', '12:00:00', '13:00:00', 'blue', 9),
+('2018-04-06', '12:00:00', '13:00:00', 'green', 1),
+('2018-04-06', '13:30:00', '15:30:00', 'green', 2),
+('2018-04-09', '8:45:00', '11:00:00', 'red', 3),
+('2018-04-09', '8:45:00', '10:00:00', 'green', 4),
+('2018-04-09', '8:45:00', '12:00:00', 'blue', 5),
+('2018-04-09', '12:50:00', '15:00:00', 'red', 6),
+('2018-04-09', '13:00:00', '15:45:00', 'blue', 7),
+('2018-04-09', '12:00:00', '13:00:00', 'red', 8),
+('2018-04-09', '12:00:00', '13:00:00', 'blue', 9),
+('2018-04-09', '12:00:00', '13:00:00', 'green', 1),
+('2018-04-09', '13:30:00', '15:30:00', 'green', 2),
+('2018-04-10', '8:45:00', '11:00:00', 'red', 3),
+('2018-04-10', '8:45:00', '10:00:00', 'green', 4),
+('2018-04-10', '8:45:00', '12:00:00', 'blue', 5),
+('2018-04-10', '12:50:00', '15:00:00', 'red', 6),
+('2018-04-10', '13:00:00', '15:45:00', 'blue', 7),
+('2018-04-10', '12:00:00', '13:00:00', 'red', 8),
+('2018-04-10', '12:00:00', '13:00:00', 'blue', 9),
+('2018-04-10', '12:00:00', '13:00:00', 'green', 1),
+('2018-04-10', '13:30:00', '15:30:00', 'green', 2),
+('2018-04-11', '8:45:00', '11:00:00', 'red', 3),
+('2018-04-11', '8:45:00', '10:00:00', 'green', 4),
+('2018-04-11', '8:45:00', '12:00:00', 'blue', 5),
+('2018-04-11', '12:50:00', '15:00:00', 'red', 6),
+('2018-04-11', '13:00:00', '15:45:00', 'blue', 7),
+('2018-04-11', '12:00:00', '13:00:00', 'red', 8),
+('2018-04-11', '12:00:00', '13:00:00', 'blue', 9),
+('2018-04-11', '12:00:00', '13:00:00', 'green', 1),
+('2018-04-11', '13:30:00', '15:30:00', 'green', 2);
+
 
 
 insert into 395db.fieldtrip_fieldtrip (title, location, info, date)
-values('Test 1', 'school', 'Random bullshit info', '2018-04-10'),
-('Test 2', 'school', 'Random bullshit info', '2018-04-11'),
-('Test 3', 'school', 'Random bullshit info', '2018-04-12'),
-('Test 4', 'school', 'Random bullshit info', '2018-04-13'),
-('Test 5', 'school', 'Random bullshit info', '2018-04-14');
+values('Waterpark', 'WEM', 'Please bring swimsuits', '2018-04-10'),
+('Park visit', 'forest place', 'Bring bug spray and food', '2018-04-11'),
+('Hospital tour', 'Glenn Rose', 'Don\'t come if sick', '2018-04-12');
