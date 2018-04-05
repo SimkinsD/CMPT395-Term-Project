7e8680883ab3ee37fabcef29cc01589b4ebb3297
John_Chubey <johnchubey@gmail.com>
2018-04-04 18:54:09 -0600

    adding the user/admin.py file back

---
+++ CMPT395/user/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from . forms import MyUserCreationForm
+from . models import MyUser, Family, Child, Volunteer, Signup
+
+class FamilyInline(admin.TabularInline):
+    model = Family
+
+# class VolunteerInline(admin.TabularInline):
+#    model = Volunteer
+
+class ChildInline(admin.TabularInline):
+    model = Child
+
+class MyUserAdmin(UserAdmin):
+    model = MyUser
+    add_form = MyUserCreationForm
+    inlines = [
+        FamilyInline,
+        # VolunteerInline,
+    ]
+    
+class FamilyAdmin(admin.ModelAdmin):
+  list_display = ('user', 'familyID', 'family_name')
+  inlines = [
+      #VolunteerInline,
+      ChildInline,
+  ]
+
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = ("volunteerID", "first_name")
+
+class ChildAdmin(admin.ModelAdmin):
+    list_display = ("childID", "family", "first_name")
+
+    
+admin.site.register(MyUser, MyUserAdmin)
+admin.site.register(Family, FamilyAdmin)
+admin.site.register(Volunteer, VolunteerAdmin)
+admin.site.register(Child, ChildAdmin)
