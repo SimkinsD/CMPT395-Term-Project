820ccf4a5e174605526b20156ce8c652f9ed4803
John_Chubey <johnchubey@gmail.com>
2018-04-06 19:58:37 -0600

    Create a table listing all of the current family's time off requests.
    There was no way for the user to see their time off requests so a table
    had to be made; I made a table in the time_off_request.html file and
    styled it in the CSS. In order to display the status properly, I had to
    create a python function to display the full name, instead of just tyhe
    letter; There are no issues other than the delete button does not yet
    work.

--- CMPT395/TimeOffRequests/views.py
+++ CMPT395/TimeOffRequests/views.py
@@ -3,16 +3,31 @@ from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 from .forms import RequestTimeOffForm
 from django.urls import reverse
+from .models import TimeOffRequest
 from user.models import Volunteer, Family
 
 # Create your views here.
 
 class TimeOffRequestView(TemplateView):
     template_name = 'time_off_request.html'
+    
+    def display_status(self, letter):
+        if(letter.lower() == "a"):
+            return "Approved"
+        elif(letter.lower() == "p"):
+            return "Pending"
+        else:
+            return "Denied"
+
 
     def get(self, request):
         request_time_off_form = RequestTimeOffForm()
-        return render(request, self.template_name, {'request_time_off_form':request_time_off_form})
+        current_time_off_requests = TimeOffRequest.objects.filter(family = Volunteer.getCurrent(self).family)
+        
+        for x in current_time_off_requests:
+            x.status = self.display_status(x.status)
+
+        return render(request, self.template_name, {'request_time_off_form':request_time_off_form, "time_off_requests": current_time_off_requests})
     
     def post(self, request):
         request_time_off_form = RequestTimeOffForm(data = request.POST, family = Volunteer.getCurrent(self).family)
@@ -21,3 +36,4 @@ class TimeOffRequestView(TemplateView):
             return redirect(reverse('weeklyCalendar'))
         
         return render(request, self.template_name, {'request_time_off_form':request_time_off_form})
+    
