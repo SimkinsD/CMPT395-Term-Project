6cce7d242cdd878f29a096544bad9b5e1afe1b63
Declan Simkins <deco.simkins@gmail.com>
2018-04-08 07:18:10 -0600

    Adds preliminary time slot and classroom models
    For use with admin tools (adding / removing classroms & modifying
    time slots)

--- CMPT395/weeklyCalendar/models.py
+++ CMPT395/weeklyCalendar/models.py
@@ -11,3 +11,18 @@ class SignUp(models.Model):
 
     def __str__(self):
         return "Classroom: " + self.classroom + "Start time: " + self.startTime.__str__() + "\nEnd Time:" + self.endTime.__str__()
+
+class TimeSlot(models.Model):
+    title = models.CharField(max_length=25)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return self.title + '\n' + self.start_time + '-' + self.end_time
+
+class Classroom(models.Model):
+    title = models.CharField(max_length=25)
+    color = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.title
