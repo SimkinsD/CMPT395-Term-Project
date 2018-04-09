40d1ab038ef0826fdb86167d533823f4a16e20e7
John_Chubey <johnchubey@gmail.com>
2018-04-08 20:46:32 -0600

    Refactor the field trip setup including creation and deletion. Also change around the urls of the field trip page.
    Instead of having a separate page to signup for field trips and viewing
    field trips I changed them both into one page and added buttons on the
    side to sign up for field trips; I created a table in the html page and
    only allowed admins to delete whole field trips. When a field trip is
    deleted, all FieldTripSignups associated with that field trip are also
    deleted. I added a few lines of code in the fieldTrips.views.py file;
    The only issue is the user has to be signed into a volunteer to sign up
    for a field trip, otherwise the page will crash.

--- CMPT395/fieldTrip/admin.py
+++ CMPT395/fieldTrip/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import FieldTripSignup, FieldTrip
 
 # Register your models here.
+admin.site.register(FieldTripSignup)
+admin.site.register(FieldTrip)
--- CMPT395/fieldTrip/views.py
+++ CMPT395/fieldTrip/views.py
@@ -1,8 +1,8 @@
 from django import forms
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from .models import *
 from .forms import *
 from user.models import *
@@ -25,7 +25,41 @@ class CurrentTrip(ListView):
         field_trips = FieldTrip.objects.all()
         field_trip_signups = FieldTripSignup.objects.all()
 
-        return render(request, self.template_name, {'field_trips':field_trips, 'field_trip_signups':field_trip_signups})
+        current_volunteer = Volunteer.getCurrent(self)
+
+        return render(request, self.template_name, {'field_trips':field_trips, 'field_trip_signups':field_trip_signups, 'current_volunteer':current_volunteer})
+    
+    def post(self, request):
+        if('add_to_field_trip' in request.POST):
+            add_volunteer = Volunteer.objects.get(volunteerID=Volunteer.getCurrent(self).volunteerID)
+            field_trip_id = request.POST.get("add_to_field_trip")
+            
+            if(FieldTripSignup.objects.filter(trip=FieldTrip.objects.get(id=field_trip_id), volunteer=add_volunteer).exists()):
+                return redirect(reverse('view_field_trip'))
+            else:
+                FieldTripSignup.objects.create(trip=FieldTrip.objects.get(id=field_trip_id), volunteer=add_volunteer)
+                return redirect(reverse('view_field_trip'))
+        
+        elif('delete_field_trip_signup' in request.POST):
+            field_trip_id = request.POST.get("delete_field_trip_signup")
+            if(FieldTripSignup.objects.filter(trip=FieldTrip.objects.get(id=field_trip_id)).exists()):
+                FieldTripSignup.objects.get(trip=FieldTrip.objects.get(id=field_trip_id), volunteer = Volunteer.objects.get(volunteerID=Volunteer.getCurrent(self).volunteerID)).delete()
+                return redirect(reverse('view_field_trip'))
+            else:
+                return redirect(reverse('view_field_trip'))
+        elif('delete_field_trip' in request.POST):
+            field_trip_id = request.POST.get("delete_field_trip")
+            if(FieldTripSignup.objects.filter(trip=FieldTrip.objects.get(id=field_trip_id)).exists()):
+                signup_list = FieldTripSignup.objects.filter(trip=FieldTrip.objects.get(id=field_trip_id))
+                for signup in signup_list:
+                    signup.delete()
+                FieldTrip.objects.get(id=field_trip_id).delete()
+                return redirect(reverse('view_field_trip'))
+            else:
+                FieldTrip.objects.get(id=field_trip_id).delete()
+                return redirect(reverse('view_field_trip'))
+
+            
 
 
 # Needs work
