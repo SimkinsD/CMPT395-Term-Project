c26ea6e42f2740dd90e007c8317fb180fe8f6f37
Declan Simkins <deco.simkins@gmail.com>
2018-04-08 22:48:31 -0600

    Updates base header to show actual hours

--- CMPT395/weeklyCalendar/templatetags/calendar_tags.py
+++ CMPT395/weeklyCalendar/templatetags/calendar_tags.py
@@ -1,4 +1,12 @@
+# Django Imports
 from django import template
+
+# Python Imports
+import datetime
+
+# App Imports
+from user import models as user_models
+from stats import views as stats_views
 
 register = template.Library()
 
@@ -27,3 +35,21 @@ def user_match(volunteer, view):
 @register.simple_tag
 def to_lower(str):
   return str.lower()
+
+@register.simple_tag
+def weekly_hours(view):
+  family = user_models.Family.objects.get(user=view.request.user)
+  day = datetime.date.today()
+  weekly_td = stats_views.FamilyStats.week_hours(view, day, family)
+  weekly_hours = weekly_td.total_seconds() // 3600
+  weekly_minutes = (weekly_td.total_seconds() - (weekly_hours * 3600)) // 60
+  return str(weekly_hours) + "h, " + str(weekly_minutes) + "m"
+
+@register.simple_tag
+def monthly_hours(view):
+  family = user_models.Family.objects.get(user=view.request.user)
+  day = datetime.date.today()
+  weekly_td = stats_views.FamilyStats.month_hours(view, day, family)
+  weekly_hours = weekly_td.total_seconds() // 3600
+  weekly_minutes = (weekly_td.total_seconds() - (weekly_hours * 3600)) // 60
+  return str(weekly_hours) + "h, " + str(weekly_minutes) + "m"
